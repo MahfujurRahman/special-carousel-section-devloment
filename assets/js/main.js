f811441dd320898922ac484e5f9e5e9847337165
Mahfujur Rahman <mahfujur705@gmail.com>
2018-04-17 23:27:07 +0600

    aghain

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+        $(".hero-area").owlCarousel({
+            items:1,
+            loop:true,
+            autoplay:false,
+            nav:false,
+            dots:true
+        });
+
+
+    });
+
+}(jQuery));	
