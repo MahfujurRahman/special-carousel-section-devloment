09a9f9e6078618005c2045342cb51eaa11751078
Mahfujur Rahman <mahfujur705@gmail.com>
2018-04-22 00:09:09 +0600

    thats are complite

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,29 @@
 
     jQuery(document).ready(function($){
 
-        $(".hero-area").owlCarousel({
-            items:1,
-            loop:true,
-            autoplay:false,
-            nav:false,
-            dots:true
-        });
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+
+       $(".hero-area").owlCarousel({
+           items:1,
+           loop:true,
+           nav:false,
+           dots:true,
+           dotsData:false,
+           dotsSpeed:1000,
+           autoplay:false,
+       });
 
 
     });
+
+    jQuery(window).load(function(){
+
+        
+    });
+
 
 }(jQuery));	
